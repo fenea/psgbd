0778de54d49cad17fa8f91f2fe1896d479b6dbb8
fenea <fenea95@gmail.com>
2017-04-13 18:39:47 +0300

    logUser package updated

--- fenea packages/logUsersPackage.sql
+++ fenea packages/logUsersPackage.sql
@@ -1,16 +1,19 @@
 create or replace Package logUser IS
 function login(usernam varchar , pass varchar) return int;
 function signup(name varchar, forename varchar,username varchar, email varchar ,pass varchar , phoneNumber varchar ) return int;
-function updatePassword(userID int,oldPass varchar, newPass varchar) return int;
+function update_password(userID int,oldPass varchar, newPass varchar) return int;
 /*function updateForename(userID int, newForename varchar) return int; */
 
 END logUser;
+
+
+
 
 CREATE OR REPLACE PACKAGE BODY logUser IS
 function login(usernam varchar , pass varchar) return int as
 passDB varchar2(256);
 begin
- select pass into passDB from Users where username like usernam;
+ select password into passDB from Users where username like usernam;
   if pass = passDB then
   return 1;
   end if;
@@ -20,6 +23,7 @@ begin
   DBMS_OUTPUT.PUT_LINE('no data found');
   return -1;
 end;
+
 function signup(name varchar, forename varchar,username varchar, email varchar ,pass varchar , phoneNumber varchar ) return int as
 begin
        /* create sequence SEQUENCE userID INCREMENT BY 1 START WITH 10000 */
@@ -29,12 +33,13 @@ when DUP_VAL_ON_INDEX then
  DBMS_OUTPUT.PUT_LINE('unique constraint violated');
   return -1;
 end;
-function updatePassword(userID int,oldPass varchar, newPass varchar) return int as
+
+function update_password(userID int,oldPass varchar, newPass varchar) return int as
 passDB varchar2(256);
 begin
- select pass into passDB from Users where ID like userID;
- if oldPass = passDB then
-  update Users set pass = newPass where ID = userID;
+ select password into passDB from Users where ID like userID;
+ if (rawtohex(dbms_crypto.hash(to_clob(oldPass), 3))) = passDB then
+  update Users set password = rawtohex(dbms_crypto.hash(to_clob(newPass), 3)) where ID = userID;
   return 1;
   end if;
   return 0;
