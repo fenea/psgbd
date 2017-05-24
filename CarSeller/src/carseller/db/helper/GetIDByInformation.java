e8826912def8e39fce9c41f3c7cdab5a2bd4216b
fenea <fenea95@gmail.com>
2017-05-25 00:52:00 +0300

    added searchByCriteria, insert in CarBean

---
+++ CarSeller/src/carseller/db/helper/GetIDByInformation.java
@@ -0,0 +1,92 @@
+package carseller.db.helper;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import carseller.db.DatabaseConnection;
+import oracle.jdbc.OracleTypes;
+
+public class GetIDByInformation {
+	
+	public static int getModel(String make, String model ){
+		int id = 0;
+		Connection connection = DatabaseConnection.getConnection();
+		String query = "SELECT ID from models where lower(make) like  lower(?) and lower(model) like lower(?)" ;
+		try{
+			CallableStatement cstmt = null;
+			cstmt = connection.prepareCall(query);
+			
+			cstmt.setString(1, make);
+			cstmt.setString(2, model);
+			ResultSet rs = cstmt.executeQuery();
+			
+			while(rs.next()){
+				id = rs.getInt(1);	
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("get model id exception at db");
+		}
+		return id;
+	}
+	
+	static public int getBodyType(String bodyType){
+		int id = 0;
+		Connection connection = DatabaseConnection.getConnection();
+		String query = "SELECT id from types where lower(BODY_TYPE)  like lower(?)";
+		try{
+			CallableStatement cstmt = null;
+			cstmt = connection.prepareCall(query);
+			cstmt.setString(1, bodyType);
+			ResultSet rs = cstmt.executeQuery();
+			while(rs.next()){
+				id = rs.getInt("id");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("get Model exception at db");
+		}
+		return id;
+	}
+	
+	static public int getFuelType(String fuelType){
+		int id = 0;
+		Connection connection = DatabaseConnection.getConnection();
+		String query = "SELECT id from fuel where lower(fuel_type) like lower_fuel(?)" ;
+		try{
+			CallableStatement cstmt = null;
+			cstmt = connection.prepareCall(query);
+			cstmt.setString(1, fuelType);
+			ResultSet rs = cstmt.executeQuery();
+			while(rs.next()){
+					id = rs.getInt("id");
+				}
+
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("get Fuel id exception at db");
+		}
+		return id;
+	}
+	static public int getColor(String color){
+		int id = 0;
+		Connection connection = DatabaseConnection.getConnection();
+		String query = "SELECT ID from colors where lower(color) like lower(?)" ;
+		try{
+			CallableStatement cstmt = null;
+			cstmt = connection.prepareCall(query);
+			cstmt.setString(1,color);
+			ResultSet rs = cstmt.executeQuery();
+			while(rs.next()){
+					id = rs.getInt("id");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("get Color id exception at db");
+		}
+		return id;
+	}
+
+}
--- CarSeller/src/carseller/db/repository/CarRepository.java
+++ CarSeller/src/carseller/db/repository/CarRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 import carseller.db.DatabaseConnection;
 import carseller.model.Car;
 import oracle.jdbc.OracleTypes;
+import carseller.db.helper.GetIDByInformation;
+import carseller.db.helper.GetInformationByID;
 
 public class CarRepository {
 
@@ -30,7 +32,7 @@ public class CarRepository {
 		car.setOwner(rs.getInt("user_id"));
 		return car;
 	}
-	
+
 	public List<Car> getAllCars(){
 		List<Car> cars = new LinkedList<>();
 		Connection connection = DatabaseConnection.getConnection();
@@ -76,5 +78,92 @@ public class CarRepository {
 
 		return cars;
 	}
-	
+
+	static public boolean insertCar(Car car){
+
+				Connection connection = DatabaseConnection.getConnection();;
+
+					String query = "insert into cars ( title, release_year, price, model_id, fuel_type ,"
+							+ " mileage,body_type_id, color ,engine_capacity,user_id,doors_number)values ( ? ,? ,?,?,?,?,?,?,?,?,?)";
+
+					try{
+						CallableStatement cstmt = null;
+						cstmt = connection.prepareCall(query);
+
+					cstmt.setString(1, car.getTitle());
+					cstmt.setInt(2, car.getYear());
+					cstmt.setDouble(3, car.getPrice());
+					cstmt.setInt(4,GetIDByInformation.getModel(car.getMake(),car.getModel()));
+					cstmt.setInt(5,GetIDByInformation.getFuelType(car.getFuel()));
+					cstmt.setInt(6, car.getMileage());
+					cstmt.setInt(7, GetIDByInformation.getBodyType(car.getBody()));
+					cstmt.setInt(8,GetIDByInformation.getColor(car.getColor()));
+					cstmt.setInt(9, car.getEngineCapacity());
+					cstmt.setInt(10, car.getOwner());
+					cstmt.setInt(11, car.getDoorNumber());
+					cstmt.executeQuery();
+				}catch(SQLException e){
+
+					System.out.println("get Make exception at db");
+					return false;
+				}
+				return true;
+			}
+
+		public static List<Car> getCarsByCriteria(SearchCriterias searchCriterias,int pageNumber){
+			List<Car> cars = new LinkedList<>();
+			Connection connection = DatabaseConnection.getConnection();
+
+			try {
+				CallableStatement cs = connection.prepareCall("{ call ? := QUERYCARS.LIST_CARS(?, ?,?,?,?,?,?) }");
+				cs.setInt(2, searchCriterias.getMinPrice());
+				cs.setInt(3, searchCriterias.getMaxPrice());
+				cs.setInt(4, searchCriterias.getMinMileage());
+				cs.setInt(5, searchCriterias.getMaxMileage());
+				cs.setInt(6, searchCriterias.getMinEngineCapacity());
+				cs.setInt(7, searchCriterias.getMaxEngineCapacity());
+				cs.setString(8,searchCriterias.getMake());
+				cs.setString(9, searchCriterias.getModel());
+				cs.setString(10, searchCriterias.getBody());
+				cs.setString(11,searchCriterias.getColor());
+				cs.setString(12, searchCriterias.getFuel());
+				cs.setInt(13, pageNumber);
+				cs.registerOutParameter(1, OracleTypes.CURSOR);
+				cs.executeUpdate();
+				ResultSet rs = (ResultSet) cs.getObject(1);
+
+
+				while (rs.next()) {
+					cars.add(CarRepository.mapTupleToCar(rs));
+				}
+
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+			return cars;
+		}
+		static public Car getCar(int id){
+			Car car = new Car();
+			Connection connection = DatabaseConnection.getConnection();
+
+			try {
+				CallableStatement cs = connection.prepareCall("select * from cars where id = ?");
+				cs.setInt(1, id);
+
+
+				ResultSet rs = cs.executeQuery();
+
+
+				while (rs.next()) {
+					car = CarRepository.mapTupleToCar(rs);
+				}
+
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+		}
+			return car;
+		}
+
 }
